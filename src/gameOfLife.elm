de84abf81fef831e6fe0724b56b456bd15362b54
harryak <f.rossmann@medienkg.de>
2015-01-31 21:56:48 +0100

    changed generation of tiles to pure HTML, excluded most styles to css

--- src/gameOfLife.elm
+++ src/gameOfLife.elm
@@ -27,10 +27,10 @@ import ListExtension as LEx
  - Config
  -----------------------------------}
 tileSize : Int
-tileSize = 14
+tileSize = 18
 
 tileGap : Int
-tileGap = 1
+tileGap = 2
 
 tileColorOn : Color.Color
 tileColorOn = Color.black
@@ -56,9 +56,10 @@ tileNumberYDir = gameHeight // tileSize
 {-----------------------------------
  - Models
  -----------------------------------}
-type alias Board = Dict.Dict (Int, Int) (Bool, GCollage.Form)
-type alias Game  = { board:Board,
-                     state:State }
+type alias Board = Dict.Dict (Int, Int) Bool
+type alias Game  =  { board: Board
+                    , state: State
+                    , speed: Float }
 type State = Paused | Playing
 
 type Update = Tile (Maybe (Int, Int)) | GameState State |Tick (Maybe ())
@@ -69,17 +70,15 @@ type Update = Tile (Maybe (Int, Int)) | GameState State |Tick (Maybe ())
 -- Use configuration to create the empty board.
 -- Implementation note: This is rather complicated and holds the GCollage.Elements for fast access
 emptyBoard : Board
-emptyBoard = LEx.cartesianProduct (,) [0..tileNumberXDir] [0..tileNumberYDir]
-                |>  List.map (
-                        \pos ->  ( pos,
-                                    ( False, getElementAt pos False )
-                                  )
-                    )
-                |>  Dict.fromList
+emptyBoard =    LEx.cartesianProduct (,) (
+                    LEx.cartesianProduct (,) [0..tileNumberXDir] [0..tileNumberYDir]
+                ) [False]
+                    |>  Dict.fromList
 
 defaultGame : Game
-defaultGame = { board = emptyBoard,
-                state = Paused }
+defaultGame =   { board = emptyBoard
+                , state = Paused
+                , speed = 0.5 }
 
 {-----------------------------------
  - Game rules
@@ -121,14 +120,14 @@ tileNeighbours (x, y) = let px = x - 1
                                        (sx, y), (sx, sy)]
 
 -- Shorthand for boolean decision whether a tile (if existing) lives. Returns thenVal if it does and elseVal if not.
-tileLivesAndThen : comparable -> Dict.Dict comparable (Bool, c) -> (Maybe a, Maybe a) -> Maybe a
+tileLivesAndThen : comparable -> Dict.Dict comparable Bool -> (Maybe a, Maybe a) -> Maybe a
 tileLivesAndThen key dict (thenVal, elseVal) =
     Dict.get key dict `Maybe.andThen` (
-        \(bool, _) -> if bool then thenVal else elseVal
+        \bool -> if bool then thenVal else elseVal
     )
 
 -- Count the tiles which are alive around the given one.
-getAliveTilesAround : (Int, Int) -> Dict.Dict (Int, Int) (Bool, c) -> Int
+getAliveTilesAround : (Int, Int) -> Dict.Dict (Int, Int) Bool -> Int
 getAliveTilesAround pos dict =
     List.sum (
         List.filterMap (
@@ -141,9 +140,9 @@ getAliveTilesAround pos dict =
 -- If the cell is alive and more than 'under', but less than 'over' neighbours live it stays
 -- alive. Otherwise it dies due do UNDER- or OVERpopulation.
 -- If the cell is dead and exactly 'repr' cells live around it, it will live due to REPRoduction
-tileWillLive : (Int, Int, Int) -> (Int, Int) -> Dict.Dict (Int, Int) (Bool, c) -> Maybe Bool
+tileWillLive : (Int, Int, Int) -> (Int, Int) -> Dict.Dict (Int, Int) Bool -> Maybe Bool
 tileWillLive (under, over, repr) pos dict =
-    Dict.get pos dict `Maybe.andThen` (\(curLives, _) ->
+    Dict.get pos dict `Maybe.andThen` (\curLives ->
         let aliveTilesAround = getAliveTilesAround pos dict
         in  if   curLives
             then if aliveTilesAround > under && aliveTilesAround < over
@@ -160,69 +159,59 @@ tileWillLiveConway = tileWillLive (1, 3, 3)
 {-----------------------------------
  - Helpers for display
  -----------------------------------}
-tilePosXDir : Int -> Float
-tilePosXDir n = 
-    let tileOffsetXDir = gameWidth // 2
-    in  (n * (tileSize + tileGap)) - tileOffsetXDir |> toFloat
+tilePosXDir : Int -> Int -> Int
+tilePosXDir x w =   let tileOffsetX = (w - gameWidth) // 2
+                    in  (x * (tileSize + tileGap)) + tileOffsetX
 
-tilePosYDir : Int -> Float
-tilePosYDir n = 
-    let tileOffsetYDir = gameHeight // 2
-    in  (n * (tileSize + tileGap)) - tileOffsetYDir |> toFloat
+tilePosYDir : Int -> Int -> Int
+tilePosYDir y h =   let tileOffsetY = (h - gameHeight) // 2
+                    in  (y * (tileSize + tileGap)) + tileOffsetY
 
 {-----------------------------------
  - Display
  -----------------------------------}
-getElementAt : (Int, Int) -> Bool -> GCollage.Form
-getElementAt (x, y) shapeFilled =
-    let
-        posX = tilePosXDir x
-        posY = tilePosYDir y
-        tileSizeFloat = tileSize |> toFloat
-        
-        shapeStyle =
-            case shapeFilled of
-                True  -> GCollage.filled tileColorOn
-                False -> GCollage.filled tileColorOff
-    in
-        [GCollage.rect tileSizeFloat tileSizeFloat
-            |> shapeStyle]
-            |> GCollage.collage tileSize tileSize
-            |> GInput.clickable (send channelTileUpdate (Tile (Just (x, y))))
-            |> GCollage.toForm
-            |> GCollage.move (posX, posY)
-
 display : (Int, Int) -> Game -> GElement.Element
 display (w, h) game =
     let _               = Debug.watch "game" game.state
-        gameOffsetYDir  = (h - gameHeight) // 2
         playing         = game.state == Playing
         buttonSymbol    = if playing then "II" else ">"
         newGameState    = if playing then (GameState Paused) else (GameState Playing)
-        playButtonStyle = HtmlAttr.style    [ ("width", "50px")
-                                            , ("height", "50px")
-                                            , ("font-size", "28pt")
-                                            , ("background-color", "lightgray")
-                                            , ("color", "darkgray")
-                                            , ("border-radius", "5px")
-                                            , ("text-align", "center")
-                                            , ("font-weight", "bolder")
-                                            , ("cursor", "pointer")]
-        sideBarStyle = HtmlAttr.style   [ ("width", "150px")
-                                        , ("height", (toString gameHeight) ++ "px")
-                                        , ("padding-left", "25px")
-                                        , ("margin-top", (toString gameOffsetYDir) ++ "px")]
         playButton = Html.div 
-                        [ playButtonStyle
+                        [ HtmlAttr.class "button"
                         , HtmlEv.onClick (send channelPlayPause newGameState)
                         ]
                         [ Html.text buttonSymbol ]
-        sideBar    = Html.div [ sideBarStyle ] [ playButton ]
+        sideBar    = Html.div
+                        [ HtmlAttr.id "sidebar"
+                        , HtmlAttr.style [("top", (toString (tilePosYDir 0 h)) ++ "px")] ]
+                        [ Html.div []
+                            [
+                              playButton
+                            ]
+                        ]
                         |> Html.toElement 150 gameHeight
     in  GElement.flow GElement.right
             [ sideBar
-            , GCollage.collage (w - 150) h
-                (List.map (\(_, elem) -> elem) (Dict.values game.board))
+            , Html.div [ HtmlAttr.id "boardContainer" ] (
+                List.map (
+                    \((x, y), alive) -> 
+                        let xPos = tilePosXDir x w
+                            yPos = tilePosYDir y h
+                        in  Html.div
+                                [ HtmlAttr.style 
+                                    [ ("left", (toString xPos) ++ "px")
+                                    , ("top", (toString yPos) ++ "px")
+                                    , ("height", (toString tileSize) ++ "px")
+                                    , ("width", (toString tileSize) ++ "px")
+                                    ]
+                                , HtmlAttr.classList
+                                    [ ("tile", True)
+                                    , ("alive", alive)
+                                    , ("dead", not alive)
+                                    ]
+                                , HtmlEv.onClick (send channelTileUpdate (Tile (Just (x, y))) )
+                                ] [ ]
+                ) (Dict.toList game.board)) |> Html.toElement gameWidth gameHeight
             ]
 
 {-----------------------------------
@@ -255,17 +244,17 @@ updateState newState game = {game | state <- newState}
 
 updateTile : (Int, Int) -> Game -> Game
 updateTile pos game =
-    let updateVal : Maybe (Bool, GCollage.Form) -> Maybe (Bool, GCollage.Form)
+    let updateVal : Maybe Bool -> Maybe Bool
         updateVal value = case value of
-                            Just (bool, _) -> Just (not bool, getElementAt pos (not bool))
-                            _              -> Nothing
+                            Just bool -> Just (not bool)
+                            _         -> Nothing
     in  {game | board <- Dict.update pos updateVal game.board}
 
 updateBoard : Game -> Game
 updateBoard game =
-    let newBoard = Dict.map  (
+    let newBoard = Dict.map (
                             \pos val -> case tileWillLiveConway pos game.board of
-                                            Just lives -> (lives, getElementAt pos lives)
+                                            Just lives -> lives
                                             _          -> val
                         ) game.board
     in  {game | board <- newBoard}
@@ -278,7 +267,7 @@ main = display <~ Window.dimensions
                 ~ foldp updateGame defaultGame (
                         mergeMany   [ subscribe channelTileUpdate
                                     , subscribe channelPlayPause
-                                    , (\time -> Tick (Just ()) ) <~ (Time.fps 0.5)
+                                    , (\_ -> Tick (Just ()) ) <~ (Time.fps 0.5)
                                     ]
                   )
                 -- ~ foldp updateTile defaultGame (subscribe channelTileUpdate)
